8acf76c38579fc0714df875923a430952370d687
Kirabo19 <kapojames@gmail.com>
2022-06-08 16:06:53 +0300

    Add index.js index.css

---
+++ modules/bookList.js
@@ -0,0 +1,12 @@
+export default function showBook() {
+  const addBookToList = document.getElementById('bk-List');
+  const addNew = document.getElementById('add_books');
+  const bookList = document.getElementById('books-list');
+  const contact = document.getElementById('contact');
+
+  bookList.classList.remove('dontShow');
+  addNew.classList.add('dontShow');
+  contact.classList.add('dontShow');
+
+  addBookToList.addEventListener('click', showBook);
+}
---
+++ modules/showForm.js
@@ -0,0 +1,12 @@
+export default function showForm() {
+  const openForm = document.getElementById('add-form');
+  const addNew = document.getElementById('add_books');
+  const bookList = document.getElementById('books-list');
+  const contact = document.getElementById('contact');
+
+  addNew.classList.remove('dontShow');
+  bookList.classList.add('dontShow');
+  contact.classList.add('dontShow');
+
+  openForm.addEventListener('click', showForm);
+}
